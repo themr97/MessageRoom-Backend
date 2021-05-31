57a764e496622121d9e05d4188bb8b48683352f9
Mahesh Ranpise <mahesh8276@gmail.com>
2021-05-31 23:25:01 +0530

    V0.2

--- server.ts
+++ server.ts
@@ -5,12 +5,11 @@ import mongoose from "mongoose";
 import User from "./models/user";
 import jwt from "jsonwebtoken";
 
+require("dotenv").config();
 const app = express();
 
 const JWT_SECRECT_TOKEN = "jfniknfihiofh2ihfi2h3fioh2fiojs9fhj9sdfhkjfoj3";
-mongoose.connect(
-  "mongodb+srv://admin:3ynp6SfeLgeHPYSx@cluster0.e59j9.mongodb.net/messageroom?retryWrites=true&w=majority"
-);
+mongoose.connect(process.env.MONGO_URL);
 
 app.use(cors());
 app.use(bodyParser.json());
